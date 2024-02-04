07de0714e5b5421f04e13cdad8452edea5e4879d
Srishti Yadav <82701079+Srishti37@users.noreply.github.com>
2024-02-04 12:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let inputBox = document.getElementById('inputBox');
+let listContainer = document.getElementById('listContainer');
+
+function addNote(){
+    if(inputBox.value === ''){
+        alert("Please add some text");
+    }else{
+        listContainer.innerHTML+= `<li>${inputBox.value}<span></li> `;
+        inputBox.value="";
+        inputBox.placeholder='Add your text';   
+    } 
+    saveData();
+}
+
+listContainer.addEventListener('click',(e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showData();
